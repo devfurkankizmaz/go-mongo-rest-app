0f5223d8a0b1eaebc5889722cdf1732fc3969b87
furkandev <dev.furkan@outlook.com>
2023-02-24 23:02:50 +0300

    route update

--- routes/movie_route.go
+++ routes/movie_route.go
@@ -6,10 +6,10 @@ import (
 )
 
 func MovieRoute(router *gin.Engine) {
-	router.POST("/films", controllers.GetFilteredData())         //filter movies with request body
-	router.POST("/film", controllers.CreateMovie())              // create a movie
-	router.GET("/films", controllers.GetAllMovies())             // get all novies
-	router.GET("/film/:userId", controllers.GetFilmByID())       //get a single movie by id
-	router.DELETE("/film/:userId", controllers.DeleteFilmByID()) //delete movie
+	router.POST("/films", controllers.GetFilteredData())          //filter movies with request body
+	router.POST("/film", controllers.CreateMovie())               // create a movie
+	router.GET("/films", controllers.GetAllMovies())              // get all novies
+	router.GET("/film/:movieId", controllers.GetFilmByID())       //get a single movie by id
+	router.DELETE("/film/:movieId", controllers.DeleteFilmByID()) //delete movie
 	router.PUT("/film", controllers.EditFilmByID())
 }
